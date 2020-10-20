5ab71e6564df3f74797566b7e67385933b5e5e80
takumi0910 <takedagen0910@gmail.com>
2020-10-20 17:07:09 +0900

    10月20日完了分

---
+++ script.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    speed: 2000,
+    spacebetween: 0,
+    autoplay: {
+        delay: 5000,
+        stopOnLastSlide: false,
+        disableOnInteraction: false,
+        reverseDirection: false
+    }
+});
